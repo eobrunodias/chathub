a8c8614d46eed42822f79c31740609b1236e3c34
Bruno Dias <brunodias40@gmail.com>
2024-08-21 12:16:24 -0300

    chore: add routes for user and topic CRUD operations

---
+++ src/backend/routes.ts
@@ -0,0 +1,79 @@
+import express, { Request, Response } from "express"
+import { Topic } from "./models/Topic"
+import { User } from "./models/User"
+
+export const routes = express.Router()
+
+routes.get("/", async (req: Request, res: Response) => {
+  res.json({ message: "Hello World" })
+})
+
+routes.post("/users", async (req: Request, res: Response) => {
+  try {
+    const { name } = req.body
+
+    const userExists = await User.findOne({ name })
+    if (userExists) {
+      return res.json(userExists)
+    }
+
+    const user = new User({ name })
+    await user.save()
+    return res.status(201).json(user)
+  } catch (err) {
+    if (err instanceof Error)
+      return res.status(400).json({ message: err.message })
+  }
+})
+
+routes.get("/topics", async (req: Request, res: Response) => {
+  try {
+    const topics = await Topic.find({}, "title")
+    return res.json(topics)
+  } catch (err) {
+    if (err instanceof Error)
+      return res.status(400).json({ message: err.message })
+  }
+})
+
+routes.get("/topics/:_id", async (req: Request, res: Response) => {
+  try {
+    const { _id } = req.params
+    const topic = await Topic.findById(_id).populate("messages.author").exec()
+    return res.json(topic)
+  } catch (err) {
+    if (err instanceof Error)
+      return res.status(400).json({ message: err.message })
+  }
+})
+
+routes.post("/topics", async (req: Request, res: Response) => {
+  try {
+    const { title } = req.body
+
+    const topic = new Topic({ title })
+    await topic.save()
+    return res.status(201).json(topic)
+  } catch (err) {
+    if (err instanceof Error)
+      return res.status(400).json({ message: err.message })
+  }
+})
+
+routes.delete("/topics/:_id", async (req: Request, res: Response) => {
+  try {
+    const { _id } = req.params
+
+    const topic = await Topic.findById(_id)
+
+    if (!topic) {
+      return res.status(404).json({ message: "topic not found" })
+    }
+
+    await topic.deleteOne()
+    return res.status(204).end()
+  } catch (err) {
+    if (err instanceof Error)
+      return res.status(400).json({ message: err.message })
+  }
+})
