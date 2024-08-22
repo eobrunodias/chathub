fe1b6a111e66d2456a0024179569422d7e768611
Bruno Dias <brunodias40@gmail.com>
2024-08-22 14:24:57 -0300

    refactor: update App component

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,7 +1,18 @@
-import { User, UserContextData } from "./@types"
 import { createContext, useState } from "react"
+import Home from "./components/Home"
 import Login from "./components/Login"
-import { Home } from "./components/Home"
+import "./index.css"
+
+export type User = {
+  _id: string
+  name: string
+}
+
+type UserContextData = {
+  user: User | null
+  login: (name: string) => Promise<void>
+  logout: () => void
+}
 
 export const UserContext = createContext<UserContextData>({} as UserContextData)
 
@@ -9,7 +20,7 @@ export function App() {
   const [user, setUser] = useState<User | null>(null)
 
   async function login(name: string) {
-    const data = await fetch("http://localhost:3333/users", {
+    const data = await fetch("http://localhost:3000/users", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ name }),
@@ -24,7 +35,9 @@ export function App() {
 
   return (
     <div className="App">
-      <h1>WebSocket</h1>
+      <h1 className="text-4xl bg-primary p-4 text-white font-bold text-center">
+        ChatHub
+      </h1>
       <UserContext.Provider value={{ user, login, logout }}>
         {user ? <Home /> : <Login />}
       </UserContext.Provider>
